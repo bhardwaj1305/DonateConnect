b97b3ce3ae3a5f4ff123b29b0fe2257c92e1a237
Bhardwaj <bhardwaj@example.com>
2026-04-07 19:52:30 +0530

    fixed firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDKXLM...",
+  apiKey: "AIzaSyDKXLmMQoLYRECXOgNw6lvyB80-0ojDsJQ",
   authDomain: "donateconnect-8a824.firebaseapp.com",
   projectId: "donateconnect-8a824",
   storageBucket: "donateconnect-8a824.firebasestorage.app",
   messagingSenderId: "524177715291",
   appId: "1:524177715291:web:fd0e8cc7b7202c6766cf7e",
+  measurementId: "G-G43NTW9ZMH"
 };
 
 export const app = initializeApp(firebaseConfig);
